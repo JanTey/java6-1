0deff40c9dcb363ae1334c00ca388b92f26e1d2a
Zhan Teyvans <teyvans@gmail.com>
2023-04-01 19:01:24 +0300

    Redesigned test with parameterization

--- src/test/java/ru/netology/services/monthsOfVocationServiceTest.java
+++ src/test/java/ru/netology/services/monthsOfVocationServiceTest.java
@@ -2,42 +2,48 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 //import ru.netology.services.monthsOfVocationService;
 
 public class monthsOfVocationServiceTest {
 
-    @Test
-    void shouldCalculateForRegisteredAndUnderLimit() {
+    @ParameterizedTest
+    @CsvSource({
+            "3, 10_000, 3_000, 20_000",
+            "2, 100_000, 60_000, 150_000"
+    })
+    void shouldCalculateForRegisteredAndUnderLimit(int expected, int income, int expenses, int threshold) {
         monthsOfVocationService service = new monthsOfVocationService();
 
         // подготавливаем данные:
-        int income = 10_000;
-        int xepenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
+//        int income = 10_000;
+//        int xepenses = 3_000;
+//        int threshold = 20_000;
+//        int expected = 3;
 
         // вызываем целевой метод:
-        long actual = service.calculate(income, xepenses, threshold);
+        int actual = service.calculate(income, expenses, threshold);
 
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void shouldCalculateForRegisteredAndOverLimit() {
-        monthsOfVocationService service = new monthsOfVocationService();
-
-        // подготавливаем данные:
-        int income = 100_000;
-        int xepenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-
-        // вызываем целевой метод:
-        long actual = service.calculate(income, xepenses, threshold);
-
-        // производим проверку (сравниваем ожидаемый и фактический):
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    void shouldCalculateForRegisteredAndOverLimit() {
+//        monthsOfVocationService service = new monthsOfVocationService();
+//
+//        // подготавливаем данные:
+//        int income = 100_000;
+//        int xepenses = 60_000;
+//        int threshold = 150_000;
+//        int expected = 2;
+//
+//        // вызываем целевой метод:
+//        long actual = service.calculate(income, xepenses, threshold);
+//
+//        // производим проверку (сравниваем ожидаемый и фактический):
+//        Assertions.assertEquals(expected, actual);
+//    }
 
 }
