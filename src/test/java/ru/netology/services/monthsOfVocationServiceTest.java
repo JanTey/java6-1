bd7ef9df813f78eca919fb9e7317a2e141aef81b
Zhan Teyvans <teyvans@gmail.com>
2023-04-01 19:17:24 +0300

    Redesigned parameterized test with data from a file

--- src/test/java/ru/netology/services/monthsOfVocationServiceTest.java
+++ src/test/java/ru/netology/services/monthsOfVocationServiceTest.java
@@ -3,17 +3,19 @@ package ru.netology.services;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 //import ru.netology.services.monthsOfVocationService;
 
 public class monthsOfVocationServiceTest {
 
     @ParameterizedTest
-    @CsvSource({
-            "3, 10_000, 3_000, 20_000",
-            "2, 100_000, 60_000, 150_000"
-    })
-    void shouldCalculateForRegisteredAndUnderLimit(int expected, int income, int expenses, int threshold) {
+//    @CsvSource({
+//            "3, 10_000, 3_000, 20_000",
+//            "2, 100_000, 60_000, 150_000"
+//    })
+    @CsvFileSource(files="src/test/resources/monthsOfVocation.csv")
+    public void shouldCalculateForRegisteredAndUnderLimit(int expected, int income, int expenses, int threshold) {
         monthsOfVocationService service = new monthsOfVocationService();
 
         // подготавливаем данные:
